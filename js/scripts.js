6e649f594b3f1ca13bd1060e1b8f9a12c1647f01
s-slingerland <seanasdfgh@gmail.com>
2017-08-19 13:22:21 -0700

    create initial functionality of website

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,41 @@
 $(document).ready(function() {
-  $("#add").submit(function() {
+  $("form#add").submit(function(event) {
     event.preventDefault();
-    var ruby1 = $("#ruby").val();
+    var ruby1 = $("select#ruby").val();
     if (ruby1 === "yes")  {
-    $(ruby).append(ruby1);
+      $(".ruby").show();
     }
-    var php1 = $("#php").val();
-    var java1 = $("#java").val();
-    var css1 = $("#css").val();
-    var c1 = $("#c").val();
+    else {
+      $(".ruby").hide();
+    }
+    var php1 = $("select#php").val();
+    if (php1 === "yes")  {
+      $(".php").show();
+    }
+    else {
+      $(".php").hide();
+    }
+    var java1 = $("select#java").val();
+    if (java1 === "yes")  {
+      $(".java").show();
+    }
+    else {
+      $(".java").hide();
+    }
+    var css1 = $("select#css").val();
+    if (css1 === "yes")  {
+      $(".css").show();
+    }
+    else {
+      $(".css").hide();
+    }
+    var c1 = $("select#c").val();
+    if (c1 === "yes")  {
+      $(".c").show();
+    }
+    else {
+      $(".c").hide();
+    }
 
     $("#results").show();
   });
