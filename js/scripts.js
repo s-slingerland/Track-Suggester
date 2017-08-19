299478d0c0e6bb1268ac9a9b47d6e48b0e2cfc44
s-slingerland <seanasdfgh@gmail.com>
2017-08-19 16:21:36 -0700

    complete functionality of js

--- js/scripts.js
+++ js/scripts.js
@@ -3,40 +3,34 @@ $(document).ready(function() {
     event.preventDefault();
     var ruby1 = $("select#ruby").val();
     if (ruby1 === "yes")  {
-      $(".ruby").show();
-    }
-    else {
-      $(".ruby").hide();
+      $("ul").append("<li>ruby!</li>");
     }
     var php1 = $("select#php").val();
     if (php1 === "yes")  {
-      $(".php").show();
-    }
-    else {
-      $(".php").hide();
+      $("ul").append("<li>php!</li>");
     }
     var java1 = $("select#java").val();
     if (java1 === "yes")  {
-      $(".java").show();
-    }
-    else {
-      $(".java").hide();
-    }
-    var css1 = $("select#css").val();
-    if (css1 === "yes")  {
-      $(".css").show();
-    }
-    else {
-      $(".css").hide();
-    }
-    var c1 = $("select#c").val();
-    if (c1 === "yes")  {
-      $(".c").show();
-    }
-    else {
-      $(".c").hide();
+      $("ul").append("<li>javascript!</li>");
     }
 
-    $("#results").show();
+    var css1 = $("select#css").val();
+    if (css1 === "yes")  {
+      $("ul").append("<li>css!</li>");
+    }
+
+    var c1 = $("select#c").val();
+    if (c1 === "yes")  {
+      $("ul").append("<li>c#!</li>");
+    }
+
+
+    if (c1 === "no" && css1 ==="no" && java1 === "no" && ruby1 === "no" && php1 === "no") {
+      $("#noResults").show();
+    
+    }
+    else $("#results").show();
+
+    })
   });
-});
+;
